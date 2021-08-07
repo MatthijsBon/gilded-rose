9cc4567aebaf65b3c7dcefc94dd0daf47efab4f0
Matthijs Bon <matthijs.bon@moxio.com>
2021-08-07 14:56:55 +0200

    Add test that over-date items degrade twice as fast

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -15,4 +15,14 @@ describe("Gilded Rose", function () {
 		}
 	});
 
+	it("should decrease ordinary items with 2 quality and 1 day after sell date", function () {
+		const gildedRose = new GildedRose([ new Item("Foo", 0, 10) ]);
+		let items = gildedRose.updateQuality();
+		expect(items[0].sellIn).to.equal(-1);
+		expect(items[0].quality).to.equal(8);
+		items = gildedRose.updateQuality();
+		expect(items[0].sellIn).to.equal(-2);
+		expect(items[0].quality).to.equal(6);
+	});
+
 });
