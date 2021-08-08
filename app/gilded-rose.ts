1be8bf4a21166cd4be7ee768c700db442cf5604e
Matthijs Bon <matthijs.bon@moxio.com>
2021-08-08 11:24:11 +0200

    Use ItemAdapterFactory to update quality of items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,3 +1,5 @@
+import { ItemAdapterFactory } from "./ItemAdapterFactory";
+
 export class Item {
 	public name: string;
 	public sellIn: number;
@@ -23,49 +25,8 @@ export class GildedRose {
 
 	public updateQuality(): Array<Item> {
 		return this.items.map((item) => {
-			if (item.name !== GildedRose.AGED_BRIE && item.name !== GildedRose.BACKSTAGE_PASSES) {
-				if (item.quality > 0) {
-					if (item.name !== GildedRose.SULFURAS) {
-						item.quality = item.quality - 1;
-					}
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-					if (item.name === GildedRose.BACKSTAGE_PASSES) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
-			if (item.name !== GildedRose.SULFURAS) {
-				item.sellIn = item.sellIn - 1;
-			}
-			if (item.sellIn < 0) {
-				if (item.name !== GildedRose.AGED_BRIE) {
-					if (item.name !== GildedRose.BACKSTAGE_PASSES) {
-						if (item.quality > 0) {
-							if (item.name !== GildedRose.SULFURAS) {
-								item.quality = item.quality - 1;
-							}
-						}
-					} else {
-						item.quality = item.quality - item.quality;
-					}
-				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-			}
+			ItemAdapterFactory.createItemAdapter(item)
+				.updateState();
 			return item;
 		});
 	}
