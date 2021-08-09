96a265143d7c62a261736710246b03025bf71ad4
Matthijs Bon <matthijs.bon@moxio.com>
2021-08-09 13:23:11 +0200

    Add simple styling / layout

--- src/App/Components/App.tsx
+++ src/App/Components/App.tsx
@@ -3,15 +3,29 @@ import "./App.css";
 
 const App = (): React.ReactElement => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
+		<div className="container">
+			<div className="header">
+				<h1>Gilded Rose Inn</h1>
+			</div>
+			<div className="main">
+				<p className="primary">
+					CONTENT
 				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			</div>
+			<div className="footer">
+				<div className="socials">
+					<div>
+						<a href="https://www.linkedin.com/in/matthijs-bon-89a3066a/" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-linkedin"/>
+						</a>
+					</div>
+					<div>
+						<a href="https://github.com/MatthijsBon/gilded-rose" target="_blank" rel="noopener noreferrer">
+							<i className="fab fa-github"/>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
